9cbe7f9ad0738ad94285e251a201b51fbcd6a595
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-10-09 00:03:37 -0700

    Refactored naming and moved common functionality to parent class.

--- main.rb
+++ main.rb
@@ -1,26 +1,27 @@
 class Bike
-    # ...
+  WEIGHT_TO_SUBTRACT = 10
+
+  def initialize(height, weight, color)
+    @height = height
+    @weight = weight
+    @color = color
   end
+
+  def get_color
+    @color
+  end
+
+  def get_height
+    @height
+  end
+
+  def decrease_weight
+    @weight -= WEIGHT_TO_SUBTRACT
+  end
+end
   
-  class RedBicycle < Bike
-  
-    AMOUNT = 10
-  
-    def initialize(a, b, c)
-      @height = a
-      @weight = b
-      @color = c
-    end
-  
-    def getColor
-      @color
-    end
-  
-    def retreive_height
-      @height
-    end
-  
-    def change_weight
-      @weight -= AMOUNT
-    end
-  end  
+class RedBike < Bike
+  def initialize(height, weight)
+    super(height, weight, 'Red')
+  end
+end  
